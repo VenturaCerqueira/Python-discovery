31bcf9a5d69060fca48bea46641e778aa56b9b32
VenturaCerqueira <82953125+VenturaCerqueira@users.noreply.github.com>
2021-10-21 11:29:38 -0300

    Add files via upload

---
+++ 03-Atividade-Soma_2-variaveis.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+num01 = float(input('Digite o primeiro valor: '))
+num02 = float(input('Digite o segundo valor :'))
+result = 0#zerando memoria de result
+result = float(num01+num02)
+print('\n',f'O resultado foi {result}.')
